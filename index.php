4b0e91c1dbc0b4b5eca4b4d04e3751e7c476be4a
mohannass <mohannassetiawan@gmail.com>
2023-06-28 06:57:59 +0700

    tambah card dan foto

--- index.php
+++ index.php
@@ -37,6 +37,16 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 
     <h1>Hello World</h1>
+    <div class="container">
+        <div class="card mb-3">
+            <img src="foto.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+            </div>
+        </div>
+    </div>
 </body>
 
 </html>
